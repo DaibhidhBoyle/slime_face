956c8a3444650f649dde4a590395001bf079bea2
DaibhidhBoyle <daibhidhboyle@gmail.com>
2023-06-28 14:16:25 +0100

    tick event handler started

--- app/index.js
+++ app/index.js
@@ -6,5 +6,13 @@ import { HeartRateSensor } from "heart-rate";
 import { battery } from "power";
 import userActivity from "user-activity";
 
-const timeHandle = document.getElementById("timeLabel"); 
+const timeHandle = document.getElementById("timeLabel");
 clock.granularity = "seconds";
+
+clock.ontick = (evt) => {
+  const now = evt.date;
+  let hours = now.getHours();
+  let mins = now.getMinutes();
+  let secs = now.getSeconds();
+
+}
