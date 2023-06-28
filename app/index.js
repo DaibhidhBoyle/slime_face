e6527a426b223c3642845d866023d9f18042eb7b
DaibhidhBoyle <daibhidhboyle@gmail.com>
2023-06-28 07:46:20 +0100

    imports and clock handlers added

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
-console.log('Hello world!');
+import document from "document";
+import clock from "clock";
+import { preferences } from "user-settings";
+import { zeroPad, } from "../common/utils";
+import { HeartRateSensor } from "heart-rate";
+import { battery } from "power";
+import userActivity from "user-activity";
+
+const timeHandle = document.getElementById("timeLabel"); 
+clock.granularity = "seconds";
